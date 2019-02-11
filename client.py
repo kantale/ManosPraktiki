b6678f6511d425f8edc90d66cba1bd51d57a5cdc
Manos Koutoulakis <tp4002@edu.teicrete.gr>
2019-02-11 15:20:31 +0200

    Dockerfile added via requests

--- client.py
+++
@@ -1,9 +0,0 @@
-import socket               # Import socket module
-
-s = socket.socket()         # Create a socket object
-host = "127.0.0.1"          # Get local machine name
-port = 8080                # Reserve a port for your service.
-
-s.connect((host, port))
-print (s.recv(1024))
-s.close()  
