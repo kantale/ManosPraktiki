e1f1e452f8bd19ab7b0602580649c847a0c28090
Manos Koutoulakis <tp4002@edu.teicrete.gr>
2019-02-12 16:46:32 +0200

    Run docker via python

--- UsingSocket/tcpClient.py
+++ UsingSocket/tcpClient.py
@@ -7,13 +7,25 @@ def Main():
     s = socket.socket()
     s.connect((host,port))
 
-    message = input("-> ")
+    message = input("Give +w (install container) or +r (run container) : ")
     while message != 'q':
-        s.send(message.encode())
-        data = s.recv(1024)
-        print("Recieved from server :" + str(data))
-        message =input("-> ")
+        if message == '+r':
+            s.send(message.encode())
+            data = s.recv(1024)
+            print("Recieved from server :" + str(data))
+            message =input("-> ")
+            s.send(message.encode())
+        elif message == '+w':
+            s.send(message.encode())
+            data = s.recv(1024)
+            print("Recieved from server :" + str(data))
+            message =input("-> ")
+            s.send(message.encode())
+        else:
+            break
     s.close()
+
+
 
 if __name__ == "__main__":
     Main()
--- UsingSocket/tcpServer.py
+++ UsingSocket/tcpServer.py
@@ -19,36 +19,41 @@ def Main():
 
     while True:
         data = c.recv(1024)
-        if not data:
+        if not data or data.decode() == '+q':
             break
-        print("from connected user :" + str(data))
+        print("from connected user :" + data.decode())
         #write in file and at the end insert \n for new line
         #dockerfile.write(str(data.decode()) + '\n')
-	#send in client message 
-        c.send("Data saved in Dockerfile".encode())
-    dockerfile.close()
+	#send in client message
+        if data.decode() == '+r':
+            c.send("Which container : ".encode())
+            data = c.recv(1024)
+            execute("sudo docker run " + data.decode())
+        elif data.decode() == '+w':
+            c.send("Give name for the container : ".encode())
+            data = c.recv(1024)
+            execute("sudo docker build -t " + data.decode() + " .")
+            execute("sudo docker run " + data.decode())
 
-    execute("sudo docker build -t testin/app .","building")
-    execute("sudo docker run testin/app","running")
+    dockerfile.close()
 
     c.close()
 
 
 
-def execute(cm, typeofcommand):
-    sudoPassword = 'tp4002'
-    process = subprocess.Popen(cm, shell=True, stdout=subprocess.PIPE,
-    stderr = subprocess.PIPE)
-    passSudo = subprocess.Popen(sudoPassword, shell=True, stdout=subprocess.PIPE,
-    stderr = subprocess.PIPE)
+def execute(cm):
+    # insert you code here
+    sudoPassword = 'INSERT_YOUR CODE'
+    process = subprocess.Popen(cm, shell=True, stdout=subprocess.PIPE,stderr = subprocess.PIPE)
+    passSudo = subprocess.Popen(sudoPassword, shell=True, stdout=subprocess.PIPE,stderr = subprocess.PIPE)
 
     out,err = process.communicate()
     errcode = process.returncode
 
     out = out.decode('utf-8')
-    print(out + " " + typeofcommand)
+    print(out + " ")
     err = err.decode('utf-8')
-    print(err + " " + typeofcommand)
+    print(err + " ")
 
 
 if __name__ == "__main__":
