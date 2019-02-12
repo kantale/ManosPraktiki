ff7f3a77b3a84d74a9f09be50fc6a38961cf34a8
Manos Koutoulakis <tp4002@edu.teicrete.gr>
2019-02-12 12:32:19 +0200

    Build Docker and after run the container

--- UsingSocket/tcpServer.py
+++ UsingSocket/tcpServer.py
@@ -1,6 +1,7 @@
 import json
 import socket
-from difflib import get_close_matches
+import subprocess
+
 def Main():
     host = "127.0.0.1"
     port = 8050
@@ -14,7 +15,7 @@ def Main():
     c, addr = s.accept()
     print("Connection from: " + str(addr))
     # Create dockerfile to write the code from requests
-    dockerfile = open('Dockerfile', 'w')
+    dockerfile = open('Dockerfile', 'r')
 
     while True:
         data = c.recv(1024)
@@ -22,11 +23,32 @@ def Main():
             break
         print("from connected user :" + str(data))
         #write in file and at the end insert \n for new line
-        dockerfile.write(str(data.decode()) + '\n')
+        #dockerfile.write(str(data.decode()) + '\n')
 	#send in client message 
         c.send("Data saved in Dockerfile".encode())
     dockerfile.close()
+
+    execute("sudo docker build -t testin/app .","building")
+    execute("sudo docker run testin/app","running")
+
     c.close()
+
+
+
+def execute(cm, typeofcommand):
+    sudoPassword = 'tp4002'
+    process = subprocess.Popen(cm, shell=True, stdout=subprocess.PIPE,
+    stderr = subprocess.PIPE)
+    passSudo = subprocess.Popen(sudoPassword, shell=True, stdout=subprocess.PIPE,
+    stderr = subprocess.PIPE)
+
+    out,err = process.communicate()
+    errcode = process.returncode
+
+    out = out.decode('utf-8')
+    print(out + " " + typeofcommand)
+    err = err.decode('utf-8')
+    print(err + " " + typeofcommand)
 
 
 if __name__ == "__main__":
